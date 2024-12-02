67f5b7d754d0af46f380ad070730405d8be61a67
standoski1 <standoskibartho@gmail.com>
2024-12-02 23:40:20 +0100

    production

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
                     />}
                     <main className="overflow-y-auto">
                       {/* <div className={`${(widthPathName.includes(pathname)) ? null : "mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10"}`}> */}
-                      <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                      <div className="w-full">
                         {children}
                       </div>
                     </main>
